3a1da166710b0af309a211ac55f137e2181e66b1
Oğuzhan Demir <oguzhandemir92@gmail.com>
2015-01-30 13:25:00 +0200

    Logger bug fixed.

--- ChromeDriverChecker/src/main/java/Managers/Logger.java
+++ ChromeDriverChecker/src/main/java/Managers/Logger.java
@@ -8,52 +8,43 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class Logger implements ILogger {
 
-    private org.apache.log4j.Logger _traceLogger;
-    private org.apache.log4j.Logger _debugLogger;
-    private org.apache.log4j.Logger _infoLogger;
-    private org.apache.log4j.Logger _warnLogger;
-    private org.apache.log4j.Logger _errorLogger;
-    private org.apache.log4j.Logger _fatalLogger;
-
+    private org.apache.log4j.Logger _logger;
+  
     public Logger() {
         PropertyConfigurator.configure("log4j.properties");
-        _traceLogger = org.apache.log4j.Logger.getLogger("trace");
-        _debugLogger = org.apache.log4j.Logger.getLogger("debug");
-        _infoLogger = org.apache.log4j.Logger.getLogger("info");
-        _warnLogger = org.apache.log4j.Logger.getLogger("warn");
-        _errorLogger = org.apache.log4j.Logger.getLogger("error");
-        _fatalLogger = org.apache.log4j.Logger.getLogger("fatal");
+        _logger = org.apache.log4j.Logger.getRootLogger();
 
-      }
+
+    }
 
     @Override
     public void trace(String log) {
-        _traceLogger.trace(log);
+        _logger.trace(log);
     }
 
     @Override
     public void debug(String log) {
-        _debugLogger.debug(log);
+        _logger.debug(log);
     }
 
     @Override
     public void info(String log) {
-        _infoLogger.info(log);
+        _logger.info(log);
     }
 
     @Override
     public void warn(String log) {
-        _warnLogger.warn(log);
+        _logger.warn(log);
     }
 
     @Override
     public void error(String log) {
-        _errorLogger.error(log);
+        _logger.error(log);
     }
 
     @Override
     public void fatal(String log) {
-        _fatalLogger.fatal(log);
+        _logger.fatal(log);
     }
 
 
